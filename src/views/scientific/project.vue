eb37f257ce26d8b4d199024a3e378a74774eba66
sobort <sobort@163.com>
2019-07-16 15:51:31 +0800

    科研项目列表搜索修改

--- src/views/scientific/project.vue
+++ src/views/scientific/project.vue
@@ -91,7 +91,7 @@
             placeholder="请输入项目名称、负责人..."
             size="small" v-model="searchInput"
           >
-            <el-button slot="append" icon="iconfont el-icon-biosearch" @click="getProjectSearchList"></el-button>
+            <el-button slot="append" icon="iconfont el-icon-biosearch" @click="getProjectList"></el-button>
           </el-input>
         </div>
       </div>
@@ -288,7 +288,8 @@ export default {
     getProjectList(){
       let obj={
         userId:this.$store.state.userId,
-        type:this.type
+        type:this.type,
+         principal:this.searchInput,
       },
       pagelist={
         offset:this.current,
@@ -304,26 +305,6 @@ export default {
         }
       })
     },
-    getProjectSearchList(){
-      let obj={
-          userId:this.$store.state.userId,
-          type:this.type,
-          principal:this.searchInput,
-        },
-        pagelist={
-          offset:this.current,
-          size:this.pageSize
-        }
-      project.getProjectList(pagelist,obj).then((res)=>{
-        if(res.returnCode==0){
-          this.tableData=res.data.modelList;
-          this.total=res.data.total;
-          console.log(this.tableData)
-        }else{
-          this.$message.error(res.msg)
-        }
-      })
-    }
   },
   mounted(){
     this.getProjectList()
